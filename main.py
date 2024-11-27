f439a908b089caa96022bb28073e1bcbf7e376bf
Prathik18 <78903165+Prathik18@users.noreply.github.com>
2024-11-27 16:53:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from pyspark.sql.functions import explode, split, collect_list, concat_ws, lit,t
 from pyspark.sql.types import StructType, StructField, IntegerType, ArrayType, DoubleType
 from pyspark.ml.linalg import SparseVector, VectorUDT, DenseVector
 from pyspark.sql import functions as F
+# os.environ["PYSPARK_PYTHON"] = r"C:\Users\DELL\anaconda3\envs\532project\python.exe"
+# os.environ["PYSPARK_DRIVER_PYTHON"] = r"C:\Users\DELL\anaconda3\envs\532project\python.exe"
 
 
 def build_dataset():
@@ -48,6 +50,7 @@ def vectorize_text(text_df):
 
     tfidf_data = idf_model.transform(featurized_data)
     tfidf_data.show()
+    return tfidf_data
     # text_df = text_df.withColumn("text", concat_ws(" ", col("title"), col("body")))
     # text_df = text_df.withColumn("text", split(col("text"), " "))
     # hash_tf = HashingTF(inputCol="title_words", outputCol="raw_features",numFeatures=20)
@@ -59,7 +62,7 @@ def vectorize_text(text_df):
     # # tfidf_data.select("post_id", "tfidf_features").show(truncate=False)
     # tfidf_data = tfidf_data.select("post_id", "tfidf_features")
     # tfidf_data.show()
-    # return tfidf_data
+   
 
 @udf(ArrayType(DoubleType()))
 def average_vectors(vectors):
@@ -67,8 +70,15 @@ def average_vectors(vectors):
     if n == 0:
         return DenseVector([]) 
     summed_vector = [0.0] * len(vectors[0])
-    for vec in vectors[1:100]:
-        summed_vector = [sum_val + vec_val for sum_val, vec_val in zip(summed_vector, vec)]
+    for vec in vectors:
+        for sum_val, vec_val in zip(summed_vector, vec):
+            print(sum_val,vec_val)
+            if vec_val != "," :
+                print(type(sum_val), type(float(vec_val)))
+        summed_vector = [
+            sum_val + float(vec_val) if vec_val != "," else sum_val
+            for sum_val, vec_val in zip(summed_vector, vec)
+        ]
     return [x / n for x in summed_vector]
     
 def convert_vector(tfidf_tuple):
@@ -105,7 +115,25 @@ if __name__ == "__main__":
     print(f"PYSPARK_DRIVER_PYTHON: {os.environ.get('PYSPARK_DRIVER_PYTHON')}")
     # Build the dataest
     tag_post_df, post_text_df_raw = build_dataset()
+    tag_post_df = tag_post_df.limit(10)
+    tag_post_df = tag_post_df.withColumn(
+    "post_ids_array",
+    F.split(F.col("post_ids"), ",").cast("array<string>")
+    )
+    tag_post_df = tag_post_df.withColumn(
+        "post_ids_array",
+        F.expr("transform(post_ids_array, x -> cast(x as int))")
+    )
+    new_post_ids = "20711943,12252506,35946365,12748096"
 
+    window_spec = Window.orderBy(F.lit(1)) 
+    tag_post_df = tag_post_df.withColumn(
+        "post_ids",
+        F.when(F.row_number().over(window_spec) <= 10, new_post_ids)  
+        .otherwise(F.col("post_ids"))
+    )
+
+    tag_post_df.show()
     # Preprocess the text data 
     post_text_df = preprocess_text(post_text_df_raw)
     post_text_df = post_text_df.filter(post_text_df["score"] >= 1)
@@ -113,30 +141,37 @@ if __name__ == "__main__":
         (trim(col("title")).isNotNull()) & (trim(col("title")) != "") & 
         (trim(col("body")).isNotNull()) & (trim(col("body")) != "")
     )
+    post_text_df = post_text_df.limit(10)
     # Vectorize the text data - (TODO: @prathik)
     vec_text_df = vectorize_text(post_text_df)
+    tag_post_df.show()
     tfidf_avg = tag_post_df.withColumn('tf_idf_vector',average_vectors(tag_post_df['post_ids']))
     average_vectors_udf = F.udf(average_vectors, ArrayType(DoubleType()))
-
+    tfidf_avg.show()
     tfidf_data = vec_text_df.withColumn(
         "dense_tfidf_features", 
         F.udf(lambda x: x.toArray().tolist(), ArrayType(DoubleType()))(F.col("tfidf_features"))
     )
+    tfidf_data.show()
     tag_post_exploded_df = tag_post_df.withColumn(
         "exploded_post_id",
         F.explode(F.col("post_ids_array"))
     )
-
+    
+    tag_post_exploded_df.limit(10)
+    tag_post_exploded_df.show()
+    tfidf_data.show()
     joined_df = tag_post_exploded_df.join(
         tfidf_data.select("post_id", "dense_tfidf_features"), 
         on=tag_post_exploded_df.exploded_post_id == tfidf_data.post_id, 
         how="inner"
     )
-    result_df = joined_df.groupBy("tag_id").agg(
-        average_vectors_udf(F.collect_list("dense_tfidf_features")).alias("avg_tfidf_features")
-    )
+    joined_df.show()
+    # result_df = joined_df.groupBy("tag_id").agg(
+    #     average_vectors_udf(F.collect_list("dense_tfidf_features")).alias("avg_tfidf_features")
+    # )
 
-    result_df.show(truncate=False)
+    # result_df.show(truncate=False)
     
     # Compute the Cosine Similarity - (TODO: @abhinav)
     cosine_similarity()
